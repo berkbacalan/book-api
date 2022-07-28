3a5f57e135402ba912706f31dcca70f7c48ec291
Berk Bacalan <berkbacalan@gmail.com>
2022-07-28 22:47:54 +0300

    implement user service endpoints

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from http import HTTPStatus
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Body
 from fastapi.exceptions import HTTPException
 from fastapi.responses import Response
 from sqlalchemy.orm import Session
+from app.auth import JWTBearer, signJWT
+from app.helper import check_user, is_user_exists
 
-from app.schema import BookRequest, Books
+from app.schema import BookRequest, Books, UserSchema, UserLoginSchema
 from .database import SessionLocal, engine
 from . import model
 
@@ -24,7 +26,7 @@ async def root():
     return "Default backend with status 200"
 
 
-@app.get('/books')
+@app.get('/books', tags=["Book"], dependencies=[Depends(JWTBearer())])
 async def get_books(db: Session = Depends(get_db)):
     try:
         books = db.query(model.Book).all()
@@ -35,7 +37,7 @@ async def get_books(db: Session = Depends(get_db)):
 
     return books
 
-@app.post('/book')
+@app.post('/book', tags=["Book"], dependencies=[Depends(JWTBearer())])
 async def save_book(book: BookRequest, db: Session = Depends(get_db)):
     '''
     Assumption: Two book can not share same name.
@@ -56,7 +58,7 @@ async def save_book(book: BookRequest, db: Session = Depends(get_db)):
     finally:
         db.close()
 
-@app.post('/delete/{book_id}')
+@app.post('/delete/{book_id}', tags=["Book"], dependencies=[Depends(JWTBearer())])
 async def delete_book(book_id: int, db: Session = Depends(get_db)):
     try:
         db.query(model.Book).filter(model.Book.id==book_id).delete()
@@ -67,7 +69,7 @@ async def delete_book(book_id: int, db: Session = Depends(get_db)):
         db.close()
         
         
-@app.put('/update/')
+@app.put('/update/', tags=["Book"], dependencies=[Depends(JWTBearer())])
 async def update_book(id: int, name: str, author:str, db: Session = Depends(get_db)):
     try:
         book = db.query(model.Book).filter(model.Book.id==id).first()
@@ -81,7 +83,7 @@ async def update_book(id: int, name: str, author:str, db: Session = Depends(get_
 
     return Response(status_code=200, content='Book updated.')
 
-@app.get('/book/{id}')
+@app.get('/book/{id}', tags=["Book"], dependencies=[Depends(JWTBearer())])
 async def get_book(id: int, db: Session = Depends(get_db)):
     try:
         book = db.query(model.Book).filter(model.Book.id==id).first()
@@ -93,3 +95,29 @@ async def get_book(id: int, db: Session = Depends(get_db)):
         raise e
     finally:
         db.close()
+
+@app.post("/user/signup", tags=["User"])
+async def create_user(user: UserSchema = Body(...), db: Session = Depends(get_db)):
+    try:
+        # check if there is user with given email address
+        user_exists = is_user_exists(user, db)
+        if user_exists:
+            return HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail="Email address already signed up.")
+        else:
+            new_user = model.User(email=user.email, password=user.password)
+            db.add(new_user)
+            db.commit()
+            db.refresh(new_user)
+    except Exception as e:
+        raise e
+    finally:
+        db.close()
+    
+    return signJWT(user.email)
+
+
+@app.post("/user/login", tags=["User"])
+async def user_login(user: UserLoginSchema = Body(...), db: Session = Depends(get_db)):
+    if check_user(user, db):
+        return signJWT(user.email)
+    return HTTPException(status_code=HTTPStatus.UNAUTHORIZED, detail="Wrong credentials.")
