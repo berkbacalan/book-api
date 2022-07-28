8f56608e293c1ce1c572979ece28cbc4d7608db1
Berk Bacalan <berkbacalan@gmail.com>
2022-07-28 20:57:05 +0300

    add db connection

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from sqlalchemy.orm import Session
+from .database import SessionLocal, engine
+from . import model
+from .config import BASE_URL, PROTOCOL
 
 app = FastAPI()
+model.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return "Default backend with status 200"
