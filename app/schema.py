632b2a440ebd269980e0ff82770a0d900e2fb662
Berk Bacalan <berkbacalan@gmail.com>
2022-07-31 19:11:50 +0300

    Update schemas

--- app/schema.py
+++ app/schema.py
@@ -1,14 +1,26 @@
+from pydantic_mongo import ObjectIdField
 from pydantic import BaseModel
 from typing import List
 
 class BookRequest(BaseModel):
     name: str
     author: str
+    category: str
+    stock: int
+    price: float
+
+
+class BookModel(BookRequest):
+    sold: int = 0
+
 
 class BookResponse(BaseModel):
-    id: int
+    id: ObjectIdField
     name: str
     author: str
+    stock: int
+    sold: int
+    price: float
 
     class Config:
         orm_mode = True
@@ -18,9 +30,20 @@ class Books(BaseModel):
     books: List[BookResponse]
 
 
+class UpdateBook(BookRequest):
+    id: str
+
+
 class UserSchema(BaseModel):
     email: str
     password: str
+
+    def __dict__(self):
+        return {
+            "email": self.email,
+            "password": self.password
+
+        }
 
     class Config:
         schema_extra = {
